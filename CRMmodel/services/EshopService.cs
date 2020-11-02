f807a29346275d126a5620e3305973c7db4c38f5
Dimitris <iracleous@gmail.com>
2020-11-02 21:17:49 +0200

    1st commit

--- CRMmodel/services/EshopService.cs
+++ CRMmodel/services/EshopService.cs
@@ -10,21 +10,30 @@ namespace CRMmodel.services
         public void MainProcess()
         {
             Console.WriteLine("CRM Application!");
+                var customer = Customer.createCustomer();
 
-            var product = Product.createProduct();
-            var customer = Customer.createCustomer();
-
-            var Order = new Order
+                var Order = new Order
             {
                 Customer = customer,
-                Products = new List<Product> { product },
+                Products = new List<Product> {  },
                 Date = DateTime.Now
             };
 
-            Console.WriteLine(product);
-            Console.WriteLine(customer);
-            product.IncreasePriceIfBelowLimit(35);
-            Console.WriteLine(product);
+            for( int i=0;i<3;i++)
+                  Order.Products.Add( Product.createProduct() );
+           
+            foreach( Product product in Order.Products)
+            {
+                Console.WriteLine(product);
+            }
+
+
+            //   Product product;
+
+            //Console.WriteLine(product);
+            //Console.WriteLine(customer);
+            //product.IncreasePriceIfBelowLimit(35);
+            //Console.WriteLine(product);
 
         }
 
